bae83a45554041ffb35b7f42048eb47718cceb64
Nikita <wolf24ru@gmail.com>
2022-08-12 09:45:38 +0700

    Split functions to get links from the 'subreddit' and 'redditor'. Create function to download image

--- Wall.py
+++ Wall.py
@@ -71,17 +71,61 @@ class UpdateWall:
         for href in link_list:
             if re.search(reg, href['href']):
                 self.href_list.append(href['href'])
-
-        try:
-            img_link = random.choice(self.href_list)
-            resource = requests.get(img_link)
-            out = open(self.file_path, 'wb')
-            out.write(resource.content)
-            out.close()
-        except IndexError:
+        if not self.href_resource:
             self._connection(self.href_resource)
-            self._download_from_resource(teg, argument, argument_main, reg)
+            self.get_link_from_web(teg, argument, argument_main, reg)
             assert f'Опять не прогрузилось попробуй снова'
+        self._download_from_resource()
+
+    def get_link_list_reddit_redditor(self):
+        users_comments = self.reddit.redditor('ze-robot').new(limit=self.limit)
+        reg = r'https://resi\.ze-robot\.com/[\w/-]*-1920%C3%971080.jpg'
+
+        for comment in users_comments:
+            for link in re.findall(reg, comment.body_html):
+                self.href_list.append(link)
+
+    def get_link_list_reddit_subreddit(self):
+        media_data = self.reddit.subreddit('wallpaper').new(limit=self.limit)
+        aspect_ratio = 16 / 9
+
+        for post in media_data:
+            if hasattr(post, 'crosspost_parent_list'):
+                # do if post have repost image
+                try:
+                    # try to get image if in post it's one
+                    images_list = post.crosspost_parent_list[0].get('preview').get('images')[0].get('source')
+                    if int(images_list['width']) / int(images_list['height']) == aspect_ratio:
+                        self.href_list.append(images_list['url'])
+                except AttributeError:
+                    # try to get image if in post they many
+                    for img in post.crosspost_parent_list[0].get('media_metadata').values():
+                        img_s = img['s']
+                        if int(img_s['x']) / int(img_s['y']) == aspect_ratio:
+                            self.href_list.append(img_s['u'])
+            else:
+                # just post
+                if hasattr(post, 'preview'):
+                    # try to get image if in post it's one
+                    images_list = post.preview.get('images')[0].get('source')
+                    if int(images_list['width']) / int(images_list['height']) == aspect_ratio:
+                        self.href_list.append(images_list['url'])
+                elif hasattr(post, 'media_metadata'):
+                    # try to get image if in post they many
+                    for img in post.media_metadata.values():
+                        img_s = img['s']
+                        if int(img_s['x']) / int(img_s['y']) == aspect_ratio:
+                            self.href_list.append(img_s['u'])
+                else:
+                    assert 'Unknown attribute. The post is not recognized'
+                    continue
+
+    def _download_from_resource(self):
+        img_link = random.choice(self.href_list)
+        resource = requests.get(img_link)
+        out = open(self.file_path, 'wb')
+        out.write(resource.content)
+        out.close()
 
     def install(self):
         match self.resource:
