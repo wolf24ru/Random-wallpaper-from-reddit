36aeb4b86072e48e206856cd42f7f3544e6f0346
Nikita <wolf24ru@gmail.com>
2022-08-31 12:15:27 +0700

    adding disappearing code from the merge

--- Wall.py
+++ Wall.py
@@ -47,6 +47,7 @@ class UpdateWall:
         self.href_list = []
         self.file_path = args.file / 'background.jpg'
         self.limit = args.limit
+        self.display_resolution = self._set_display_resolution(args.display_resolution)
         self.reddit = praw.Reddit(
             client_id=secret.client_id,
             client_secret=secret.client_secret,
@@ -61,6 +62,30 @@ class UpdateWall:
             print(f'Ваш ресурс еще не добавлен, в качестве ресурса по умолчанию'
                   f'Установлен ze-robot')
 
+    @staticmethod
+    def _display_resolution_valid(resolution: str) -> bool:
+        display_resolution_list = ['1920x1080', '1600x900', '1366x768', '1280x720',
+                                   '1280x800', '1024x768', '1280x1024', '768x1024',
+                                   '720x480', '352x240', '352x288', '352x480',
+                                   '352x576', '480x480', '480x576', '528x480',
+                                   '544x480', '544x576', '640x480', '704x480',
+                                   '704x576', '720x576', '1280x1080', '3440x1440',
+                                   '2560x1080', '3840x2160', '2560x1440', '2560x1600',
+                                   '1920x1200', '1600x1200', '1080x1920', '720x1280',
+                                   '720x1480', '1080x2220'
+                                   ]
+
+        if resolution in display_resolution_list:
+            return True
+        return False
+
+    def _set_display_resolution(self, display_resolution):
+        if self._display_resolution_valid(display_resolution):
+            return display_resolution
+        assert 'unvalid display resolution'
+        config = read_config()
+        return config['display']['resolution']
+
     def _os_system(self):
         """Проверка установленной операционной системы"""
         match platform.system():
@@ -69,22 +94,22 @@ class UpdateWall:
             case 'Windows':
                 ctypes.windll.user32.SystemParametersInfoW(20, 0, str(self.file_path), 3)
 
-    def __set_kde_wallpaper(self):
-        plugin = 'org.kde.image'
-        jscript = f"""
-        var allDesktops = desktops();
-        print (allDesktops);
-        for (i=0;i<allDesktops.length;i++) {{
-            d = allDesktops[i];
-            d.wallpaperPlugin = "{plugin}";
-            d.currentConfigGroup = Array("Wallpaper", "{plugin}", "General");
-            d.writeConfig("Image", "file://{self.file_path}")
-        }}
-        """
-        bus = dbus.SessionBus()
-        plasma = dbus.Interface(bus.get_object(
-            'org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
-        plasma.evaluateScript(jscript)
+    # def __set_kde_wallpaper(self):
+    #     plugin = 'org.kde.image'
+    #     jscript = f"""
+    #     var allDesktops = desktops();
+    #     print (allDesktops);
+    #     for (i=0;i<allDesktops.length;i++) {{
+    #         d = allDesktops[i];
+    #         d.wallpaperPlugin = "{plugin}";
+    #         d.currentConfigGroup = Array("Wallpaper", "{plugin}", "General");
+    #         d.writeConfig("Image", "file://{self.file_path}")
+    #     }}
+    #     """
+    #     bus = dbus.SessionBus()
+    #     plasma = dbus.Interface(bus.get_object(
+    #         'org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
+    #     plasma.evaluateScript(jscript)
 
     def _linux_release(self):
         """"Проверка рабочего окружения и выдача команды на изменение обоев"""
@@ -108,9 +133,9 @@ class UpdateWall:
                     .stdout.decode('utf-8')
                 os.system(f"xfconf-query -c xfce4-desktop -p {desktop_wallpaper} -s {self.file_path}")
             case 'KDE':
-                # Та же ситуация, что и с LXQt меняет только один раз и заме не меняет изображение при том же
-                # названии файла
                 print('Currently not implemented')
+                # Та же ситуация, что и с LXQt меняет только один раз и выполнении не меняет изображение при том же
+                # названии файла
                 # self.__set_kde_wallpaper()
             case _:
                 assert 'Неизвестное графическое окружение'
@@ -140,8 +165,9 @@ class UpdateWall:
     def get_link_list_reddit_redditor(self):
         # TODO оптимизировать и сделать более универсальным
         """"Получение ссылок изображений с постов пользователя"""
-        users_comments = self.reddit.redditor('ze-robot').new(limit=self.limit)
-        reg = r'https://resi\.ze-robot\.com/[\w/-]*-1920%C3%971080.jpg'
+        users_comments = self.reddit.redditor(self.resource).new(limit=self.limit)
+        horizontal, vertical = self.display_resolution.split('x')
+        reg = fr'https://resi\.ze-robot\.com/[\w/-]*-{horizontal}%C3%97{vertical}.jpg'
 
         for comment in users_comments:
             for link in re.findall(reg, comment.body_html):
@@ -186,9 +212,16 @@ class UpdateWall:
 
     def _download_from_resource(self):
         """Рандомный выбор картинки и её загрузка"""
+        if not self.href_list:
+            assert ('No images matching your parameters were found.'
+                    'Try different resolution settings for the image.')
+            print('No images matching your parameters were found.'
+                  'Try different resolution settings for the image.')
+            exit()
+
         img_link = random.choice(self.href_list)
         resource = requests.get(img_link)
-
+        print('')
         out = open(self.file_path, 'wb')
         out.write(resource.content)
         out.close()
@@ -211,6 +244,27 @@ class UpdateWall:
         self._os_system()
 
 
+def save_config(config, arg):
+    config['main']['limit'] = arg.limit
+    config['main']['file_name'] = arg.file_name
+    config['main']['file_path'] = str(arg.file)
+    config['main']['resource'] = arg.resource
+
+    config['display']['resolution'] = arg.display_resolution
+    # config['display']['ratio'] = arg.ratio
+    PATH = pathlib.Path(__file__).parent.absolute()
+    with open(PATH / 'config.yaml', 'w') as f:
+        yaml.dump(config, stream=f,
+                  default_style=False, sort_keys=False)
+
+
+def read_config():
+    PATH = pathlib.Path(__file__).parent.absolute()
+    with open(PATH / 'config.yaml') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    return config
+
+
 def args_error(error):
     """Вывод ошибки при некорректных аргументах"""
     print(f'Error: {error}')
@@ -218,28 +272,47 @@ def args_error(error):
 
 def main(argv):
     """Создание аргументов и запуск программы"""
-    LIMIT = 15
-    PATH = pathlib.Path(__file__).parent.absolute()
+    config = read_config()
+
+    limit = config['main']['limit']
+    file_name = config['main']['file_name']
+    file_path = pathlib.Path(config['main']['file_path'])
+    if file_path == pathlib.Path('.'):
+        file_path = pathlib.Path(__file__).parent.absolute()
+    resource = config['main']['resource']
+
+    resolution = config['display']['resolution']
+    ratio = config['display']['ratio']
+
+    # PATH = pathlib.Path(__file__).parent.absolute()
 
     parser = argparse.ArgumentParser(description='Load wallpaper from reddit', prog='Random_wallpaper')
     parser.error = args_error
 
     parser.add_argument('-r', '--resource',
-                        help='selecting a resource to upload',
+                        help='selecting a resource to upload\n',
                         choices=['ze-robot', 'r-wallpaper', 'all'],
-                        default='ze-robot')
+                        default=resource)
     parser.add_argument('-l', '--limit',
                         type=int, help='quantity limit for sampling',
-                        default=LIMIT)
+                        default=limit)
     parser.add_argument('-f', '--file', type=pathlib.Path, help='path to save file ',
-                        default=PATH,
+                        default=file_path,
                         metavar='path/to/folder')
+    parser.add_argument('--file-name', help='file name',
+                        default=file_name)
+    parser.add_argument('-d', '--display-resolution', help='display resolution',
+                        default=resolution,)
+    parser.add_argument('-c', '--config-file', help='change config file',
+                        action='store_true', default=False)
     parser.add_argument('-v', '--version', action='version',
-                        version='%(prog)s 1.1', help='program version')
+                        version='%(prog)s 1.1', help='program version\n')
 
     args = parser.parse_args()
-    test = UpdateWall(args)
-    test.install()
+    if args.config_file:
+        save_config(config, args)
+    wallpaper = UpdateWall(args)
+    wallpaper.install()
 
 
 if __name__ == '__main__':
